27556fe1d42ee86b94b7106b056d55a599180209
c0chenji <jarick@live.ca>
2019-01-17 21:30:24 -0500

    test

--- myAlbum/wsgi.py
+++ myAlbum/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for myAlbum project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
+import signal
 
+import sys
+import traceback
+
+import time
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myAlbum.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
